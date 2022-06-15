fd86916f370a9259dfde23df97bd1228051ef89b
bebe3 <i.masashi0323@gmail.com>
2022-06-15 16:17:11 +0900

    change priority and install inspector

--- src/plugins/wrapTextInSpan.js
+++ src/plugins/wrapTextInSpan.js
@@ -6,61 +6,79 @@ export default class wrapTextInSpan extends Plugin {
 		const editor = this.editor;
 		const conversion = editor.conversion;
 
-		/*
 		const schema = editor.model.schema;
 		schema.extend( '$text', { allowAttributes: [ 'emptySpan' ] } );
 
+		conversion.for( 'upcast' ).add( dispatcher => {
+			dispatcher.on( 'text', ( evt, data, conversionApi ) => {
+				const {
+					consumable,
+					writer,
+					safeInsert,
+					convertChildren,
+					updateConversionResult
+				} = conversionApi;
+
+				const { viewItem } = data;
+
+				if ( viewItem.parent.name !== "p" ) {
+					return;
+				}
+				console.log(viewItem);
+			})
+				
+		})
+
 		// Handle empty spans
 		//  Add an upcast (view-to-model) converter for empty span.
-		conversion.for( 'upcast' ).elementToAttribute( {
-			view: {
-				name: 'span',
-				styles: false,
-				attributes: false
-			},
-			model: {
-				key: 'emptySpan',
-				value: true
-			},
-			converterPriority: 'high'
-		} );
+		// conversion.for( 'upcast' ).elementToAttribute( {
+		// 	view: {
+		// 		name: 'span',
+		// 		styles: false,
+		// 		attributes: false
+		// 	},
+		// 	model: {
+		// 		key: 'emptySpan',
+		// 		value: true
+		// 	},
+		// 	converterPriority: 'high'
+		// } );
 
 		// Add an downcast (model-to-view) converter for empty span.
-		conversion.for( 'downcast' ).attributeToElement( {
-			model: 'emptySpan',
-			view: ( modelAttributeValue, viewWriter ) => {
-				return viewWriter.createAttributeElement( 'span', {},
-					{ priority: 12 } );
-			},
-			converterPriority: 'high'
-		} );
-
-		 */
+		// conversion.for( 'downcast' ).attributeToElement( {
+		// 	model: 'emptySpan',
+		// 	view: ( modelAttributeValue, viewWriter ) => {
+		// 		return viewWriter.createAttributeElement( 'span', {},
+		// 			{ priority: 12 } );
+		// 	},
+		// 	converterPriority: 'high'
+		// } );
 
 		// Add low-level converter only for data taken out of the editor.
 		// It will wrap text into a span.
-		conversion.for( 'dataDowncast' ).add( dispatcher => {
-			dispatcher.on( 'insert:$text', ( evt, data, conversionApi ) => {
-				// If text has any attributes stop processing as these attributes
-				// will be converted to elements wrapping text in view.
-				if (!data.item.getAttributes().next().done) {
-					return;
-				}
-				if (!conversionApi.consumable.consume( data.item, 'insert' )) {
-					return;
-				}
-				const viewWriter = conversionApi.writer;
-				// create position in view based on range
-				const viewPosition = conversionApi.mapper.toViewPosition( data.range.start );
-				// create text in view based on input data
-				const viewText = viewWriter.createText( data.item.data );
-				// create wrapper span
-				const span = viewWriter.createAttributeElement( 'span' );
-				// insert view text at the specified view postion
-				viewWriter.insert( viewWriter.createPositionAt( viewPosition.parent, 'end' ), viewText );
-				// wrap view text in span
-				viewWriter.wrap( viewWriter.createRangeOn( viewText ), span );
-			}, { priority: 'low' } );
-		} );
+		// conversion.for( 'dataDowncast' ).add( dispatcher => {
+		// 	dispatcher.on( 'insert:$text', ( evt, data, conversionApi ) => {
+		// 		console.log(data);
+		// 		// If text has any attributes stop processing as these attributes
+		// 		// will be converted to elements wrapping text in view.
+		// 		if (!data.item.getAttributes().next().done) {
+		// 			return;
+		// 		}
+		// 		if (!conversionApi.consumable.consume( data.item, 'insert' )) {
+		// 			return;
+		// 		}
+		// 		const viewWriter = conversionApi.writer;
+		// 		// create position in view based on range
+		// 		const viewPosition = conversionApi.mapper.toViewPosition( data.range.start );
+		// 		// create text in view based on input data
+		// 		const viewText = viewWriter.createText( data.item.data );
+		// 		// create wrapper span
+		// 		const span = viewWriter.createAttributeElement( 'span' );
+		// 		// insert view text at the specified view postion
+		// 		viewWriter.insert( viewWriter.createPositionAt( viewPosition.parent, 'end' ), viewText );
+		// 		// wrap view text in span
+		// 		viewWriter.wrap( viewWriter.createRangeOn( viewText ), span );
+		// 	}, { priority: 'low' } );
+		// } );
 	}
 }
